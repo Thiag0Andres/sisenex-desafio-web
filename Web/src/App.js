ad89830eb0392f69e56a79806f6be41968be7f3c
Thiag0Andres <thiagopalacios@eng.ci.ufpb.br>
2020-03-24 21:41:47 -0300

    Update App.js

--- Web/src/App.js
+++ Web/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import api from './services/api';
 
 import './global.css';
-import './App.css';
+import './App.css'; 
 import './Sidebar.css';
 import './Main.css';
 import './Searchbar.css';
